9a2356c151f8b35f0aa596c35e4790c5a92d762d
Aerkza <aerkza_ac129@hotmail.com>
2019-11-21 12:39:02 +0700

    Walk Anim and More Blocking

--- 2D game/Assets/Scripts/PlayerMovement.cs
+++ 2D game/Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public int playerSpeed = 10;
-	private bool facingRight = false;
+	//private bool facingRight = false;
     private float moveX;
     public bool canMove;
+    //public AudioSource FootStep;
 
     void Start ()
     {
@@ -28,23 +29,32 @@ public class PlayerMovement : MonoBehaviour {
 		//Player Control
 		moveX = Input.GetAxis ("Horizontal");
 		//Animations
-
+        if (moveX != 0)
+        {
+            
+            GetComponent<Animator>().SetBool("IsWalking", true);
+        } else {
+            //FootStep.Play();
+            GetComponent<Animator>().SetBool("IsWalking", false);
+        }
 		//Player Direction
-		if (moveX < 0.0f && facingRight == false) {
-			FlipPlayer ();
-		} else if (moveX > 0.0f && facingRight == true) {
-			FlipPlayer ();
-		}
+		if (moveX < 0.0f) {
+            GetComponent<SpriteRenderer>().flipX = true;
+			//FlipPlayer ();
+		} else if (moveX > 0.0f) {
+            GetComponent<SpriteRenderer>().flipX = false;
+            //FlipPlayer ();
+        }
 
 		//Physics
 		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
 	}
 
-	void FlipPlayer()
+	/*void FlipPlayer()
     {
 		facingRight = !facingRight;
 		Vector2 localScale = gameObject.transform.localScale;
 		localScale.x *= -1;
 		transform.localScale = localScale;
-	}
+	}*/
 }
