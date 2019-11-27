60dff5107620f0b81ebc29f4994d07e771bae80c
Aerkza <aerkza_ac129@hotmail.com>
2019-11-27 17:12:33 +0700

    MiscUpdate

---
+++ 2D game/Assets/Scripts/UI/DelayEndPic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayEndPic : MonoBehaviour
+{
+    public GameObject content;
+    
+    void Start()
+    {
+        StartCoroutine(DelayEndPicture());
+    }
+
+    IEnumerator DelayEndPicture()
+    {
+        yield return new WaitForSeconds(2);
+        content.SetActive(true);
+    }
+}
---
+++ 2D game/Assets/Scripts/UI/RestartCheckPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartCheckPoint : MonoBehaviour {
+
+    public GameObject FadeIn;
+
+    public void Restart()
+	{
+        FadeIn.SetActive(true);
+        StartCoroutine(DelaySceneLoad());
+	}
+
+	IEnumerator DelaySceneLoad()
+	{
+		yield return new WaitForSeconds(3);
+		SceneManager.LoadScene("EndRoom");
+	}
+}
