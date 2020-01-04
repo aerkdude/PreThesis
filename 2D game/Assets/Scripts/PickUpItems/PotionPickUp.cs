321eff79a9e5f94e2c8520014432ed81d7c18c40
Aerkza <aerkza_ac129@hotmail.com>
2020-01-04 08:16:24 +0700

    Update01

---
+++ 2D game/Assets/Scripts/PickUpItems/PotionPickUp.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PotionPickUp : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject OpenPanel = null;
+    public GameObject PaperButton;
+    //public GameObject tutorialBoxThree;
+    public GameObject doorDialougeZone;
+    public GameObject doorToLivingRoom;
+    public GameObject HealthBar;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    private bool IsOpenPanelActive
+    {
+        get
+        {
+            return OpenPanel.activeInHierarchy;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            OpenPanel.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            OpenPanel.SetActive(false);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (IsOpenPanelActive)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                for (int i = 0; i < inventory.slots.Length; i++)
+                {
+                if (inventory.isFull[i] == false)
+                    inventory.isFull[i] = true;
+                    PaperButton.SetActive(true);
+                    //tutorialBoxThree.SetActive(true);
+                    HealthBar.SetActive(true);
+                    doorDialougeZone.SetActive(false);
+                    doorToLivingRoom.SetActive(true);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
+ 
