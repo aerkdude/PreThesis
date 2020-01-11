b3abb5ea0371da7ee15962d6a5a581169aaabeaf
Aerkza <aerkza_ac129@hotmail.com>
2020-01-11 17:53:11 +0700

    Lock Res and Add Skippable Intro

---
+++ 2D game/Assets/Scripts/ToNextLevel/SkipToBedroom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipToBedroom : MonoBehaviour
+{
+   public GameObject FadeIn;
+    
+   void Update()
+   {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            FadeIn.SetActive(true);
+            StartCoroutine(LoadNextScene());
+        }
+   }
+
+    IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("BedRoom");
+    }
+}
