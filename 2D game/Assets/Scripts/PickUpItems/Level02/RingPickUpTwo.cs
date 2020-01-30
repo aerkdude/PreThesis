e803b07b556adc662367a45733ee99c467c8b184
Aerkza <aerkza_ac129@hotmail.com>
2020-01-30 10:42:06 +0700

    Update02

---
+++ 2D game/Assets/Scripts/PickUpItems/Level02/RingPickUpTwo.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingPickUpTwo : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject OpenPanel = null;
+    public GameObject RingButton;
+    public GameObject invWall;
+    public Dialouge dialouge;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    private bool IsOpenPanelActive
+    {
+        get
+        {
+            return OpenPanel.activeInHierarchy;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            OpenPanel.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            OpenPanel.SetActive(false);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (IsOpenPanelActive)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                for (int i = 0; i < inventory.slots.Length; i++)
+                {
+                if (inventory.isFull[i] == false)
+                   inventory.isFull[i] = true;
+                    FindObjectOfType<DialougeManagerTestTwo>().StartDialouge(dialouge);
+                    RingButton.SetActive(true);
+                    gameObject.SetActive(false);
+                    invWall.SetActive(true);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
+ 
---
+++ 2D game/Assets/Scripts/ToNextLevel/LevelTwo/DoorToM.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorToM : MonoBehaviour
+{
+    public GameObject FadeIn;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            FadeIn.SetActive(true);
+            StartCoroutine(LoadNextScene());
+        }
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("MRoom");
+    }
+}
