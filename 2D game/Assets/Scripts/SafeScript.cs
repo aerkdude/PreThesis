bd2668a70e997df8cfffb549da989297195f4c93
Aerkdude <aerkza_ac129@hotmail.com>
2020-02-13 15:49:52 +0700

    Add padlock

---
+++ 2D game/Assets/Scripts/SafeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeScript : MonoBehaviour
+{
+    public GameObject OpenPanel;
+    public GameObject Safe;
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        OpenPanel.SetActive(false);
+        Safe.SetActive(true);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        //OpenPanel.SetActive(true);
+        Safe.SetActive(false);
+    }
+}
