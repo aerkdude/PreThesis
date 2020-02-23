a5d324c2786c7affb4b77cc0ec883d546f39a032
Aerkza <aerkza_ac129@hotmail.com>
2020-02-23 17:58:28 +0700

    Update

---
+++ 2D game/Assets/Scripts/UI/ButtonSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip hoveFx;
+    public AudioClip clickFx;
+
+    public void HoverSound()
+    {
+        myFx.PlayOneShot(hoveFx);
+    }
+    public void ClickSound()
+    {
+        myFx.PlayOneShot(clickFx);
+    }
+}
