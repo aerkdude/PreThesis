aaf4ca83fab044d26293ae8b3a7466dbd212a8ca
Aerkza <aerkza_ac129@hotmail.com>
2020-02-27 15:27:02 +0700

    UpdateLvL2

---
+++ 2D game/Assets/Scripts/UI/RestartLV2To2Script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLV2To2Script : MonoBehaviour {
+
+    public GameObject FadeIn;
+
+	public void Restart()
+	{
+        FadeIn.SetActive(true);
+		StartCoroutine(DelaySceneLoad());
+	}
+
+	IEnumerator DelaySceneLoad()
+	{
+		yield return new WaitForSeconds(1.0f);
+		SceneManager.LoadScene("BedRoomLvl2to2");
+	}
+}
