e0656ccb027ffb55bee96888aa6dc9da84129fc9
Aerkdude <aerkza_ac129@hotmail.com>
2020-04-17 21:17:05 +0700

    Update

---
+++ 2D game/Assets/Scripts/UI/EndCredits.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCredits : MonoBehaviour
+{
+    public GameObject endText;
+    public GameObject devText;
+    public GameObject fadeBlack;
+
+    void Start()
+    {
+        StartCoroutine(EndTextShow());
+    }
+
+    IEnumerator EndTextShow()
+    {
+        yield return new WaitForSeconds(3);
+        endText.SetActive(true);
+        StartCoroutine(DevTextShow());
+    }
+
+    IEnumerator DevTextShow()
+    {
+        yield return new WaitForSeconds(8);
+        devText.SetActive(true);
+        StartCoroutine(FadeBlackScreen());
+    }
+
+    IEnumerator FadeBlackScreen()
+    {
+        yield return new WaitForSeconds(9);
+        fadeBlack.SetActive(true);
+        StartCoroutine(LoadMenu());
+    }
+
+    IEnumerator LoadMenu()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
